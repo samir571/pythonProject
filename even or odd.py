601152254437283d55513400c559e44986234f20
samir571 <sameershrestha68@gmail.com>
2021-02-10 12:26:39 +0545

    addition

---
+++ even or odd.py
@@ -0,0 +1,6 @@
+num = int(input('enter the number:'))
+s = num % 2
+if (s == 0):
+    print('it is an even number')
+else:
+    print('it is an odd number')
---
+++ first.py
@@ -0,0 +1,4 @@
+radius=float(input('enter the radius:'))
+pi = 22 / 7
+area = pi * radius ** 2
+print('the are of circle is',area)
